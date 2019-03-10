adf121e4cd0f32bc4f8fbc8160a5f19f82ca306a
Diego Firpo Marchelli <diegofirpo33@gmail.com>
2019-03-10 14:06:15 -0300

    Finished the cryptocurrency frontend.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
     state = { walletInfo: {} };
 
     componentDidMount() {
-        fetch('http://localhost:3000/api/wallet-info')
+        fetch(`${document.location.origin}/api/wallet-info`)
             .then(response => response.json())
             .then(json => this.setState({ walletInfo: json }));
     }
@@ -17,6 +17,16 @@ class App extends Component {
                 <div>
                     Welcome to the blockchain...
                 </div>
+                <br/>
+                <div>
+                    <Link to='/blocks'>Blocks</Link>
+                </div>
+                <div>
+                    <Link to='/conduct-transaction'>Conduct a Transaction</Link>
+                </div>
+                <div>
+                    <Link to='/transaction-pool'>Transaction Pool</Link>
+                </div>
                 <br />
                 <div className="WalletInfo">
                     <div>
@@ -26,8 +36,6 @@ class App extends Component {
                         Balance: {balance}
                     </div>
                 </div>
-                <br />
-                <Blocks />
             </div>
         );
     }
